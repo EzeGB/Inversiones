7cf0ae033f93b2556941f2a60f1cba0422fce4b0
Ezequiel Gerstel Bodoha <ezequielgerstel777@gmail.com>
2023-11-17 18:16:36 -0400

    simularAF_AC_FAI

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Pesimista: ");
-        double pesimista = scan.nextDouble();
-        System.out.println("Probable: ");
-        double probable = scan.nextDouble();
-        System.out.println("Optimista: ");
-        double optimista = scan.nextDouble();
-        for (int i = 0; i<10; i++) {
-            System.out.println("Se simulo: " + simularTriangular(pesimista,probable,optimista));
+
+        System.out.println("Ingrese estimaciones AF");
+        double[] estimacionesAF = recolectarEstimaciones(scan);
+        System.out.println("Ingrese estimaciones AC");
+        double[] estimacionesAC = recolectarEstimaciones(scan);
+        System.out.println("Ingrese estimaciones FAI");
+        double[] estimacionesFAI = recolectarEstimaciones(scan);
+
+        System.out.println("Se simulo para AF:" + simularTriangular(estimacionesAF));
+        System.out.println("Se simulo para AC:" + simularTriangular(estimacionesAC));
+
+        for (int i = 0; i<5; i++) {
+            System.out.println("Se simulo para FAI en año " + (i+1) + ": " +simularTriangular(estimacionesFAI));
         }
     }
-    public static double simularTriangular(double pesimista, double probable, double optimista){
-        double relacion = (probable-pesimista)/(optimista-pesimista);
+    public static double simularTriangular(double[] estimaciones){
+        double a = estimaciones[0];
+        double b = estimaciones[1];
+        double c = estimaciones[2];
+
+        double relacion = (b - a)/(c - a);
         double R = Math.random();
         if (R <= relacion){
-            return (pesimista + Math.sqrt((optimista-pesimista)*(probable-pesimista)*R));
+            return (a + Math.sqrt((c - a)*(b - a)*R));
         } else {
-            return (optimista - Math.sqrt((optimista-pesimista)*(optimista-probable)*(1-R)));
+            return (c - Math.sqrt((c - a)*(c - b)*(1-R)));
         }
+    }
+
+    public static double[] recolectarEstimaciones(Scanner scan){
+        double [] datos = new double[3];
+        System.out.println("Pesimista: ");
+        datos[0] = scan.nextDouble();
+        System.out.println("Probable: ");
+        datos[1]  = scan.nextDouble();
+        System.out.println("Optimista: ");
+        datos[2]  = scan.nextDouble();
+        return datos;
     }
 
 }
