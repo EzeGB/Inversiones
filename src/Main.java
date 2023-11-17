157574914002812ddba6e69c3a1d1c04ceef99ce
Ezequiel Gerstel Bodoha <ezequielgerstel777@gmail.com>
2023-11-17 17:57:52 -0400

    lectorDatos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 
 import java.lang.Math;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(simularTriangular(-100000, -70000,-60000));
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pesimista: ");
+        double pesimista = scan.nextDouble();
+        System.out.println("Probable: ");
+        double probable = scan.nextDouble();
+        System.out.println("Optimista: ");
+        double optimista = scan.nextDouble();
+        for (int i = 0; i<10; i++) {
+            System.out.println("Se simulo: " + simularTriangular(pesimista,probable,optimista));
+        }
     }
     public static double simularTriangular(double pesimista, double probable, double optimista){
         double relacion = (probable-pesimista)/(optimista-pesimista);
         double R = Math.random();
-
         if (R <= relacion){
             return (pesimista + Math.sqrt((optimista-pesimista)*(probable-pesimista)*R));
         } else {
