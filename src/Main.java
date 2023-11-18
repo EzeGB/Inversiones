fa2f23a394b434483d9c8252bea0cbd17748c78d
Ezequiel Gerstel Bodoha <ezequielgerstel777@gmail.com>
2023-11-17 22:12:24 -0400

    recoleccionDatosCompleta

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 
 import java.lang.Math;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Ingrese estimaciones AF");
-        double[] estimacionesAF = recolectarEstimaciones(scan);
-        System.out.println("Ingrese estimaciones AC");
-        double[] estimacionesAC = recolectarEstimaciones(scan);
-        System.out.println("Ingrese estimaciones FAI");
-        double[] estimacionesFAI = recolectarEstimaciones(scan);
-
-        System.out.println("Se simulo para AF:" + simularTriangular(estimacionesAF));
-        System.out.println("Se simulo para AC:" + simularTriangular(estimacionesAC));
-
-        for (int i = 0; i<5; i++) {
-            System.out.println("Se simulo para FAI en año " + (i+1) + ": " +simularTriangular(estimacionesFAI));
-        }
+        double AF = simularTriangular(recolectarEstimaciones(scan, "AF"));
+        double AC = simularTriangular(recolectarEstimaciones(scan, "AC"));
+        double[] estimacionesFAI = recolectarEstimaciones(scan, "FAI");
+        double [] FAI = new double[5];
+            for (int i = 0; i<5; i++) {
+                FAI[i]=simularTriangular(estimacionesFAI);
+            }
+        ArrayList<Double> Inf = new ArrayList<Double>();
+        Inf.add(simularTriangular(recolectarEstimaciones(scan, "Inf1")));
+        Inf.add(simularTriangular(recolectarEstimaciones(scan, "Inf2")));
+        Inf.add(simularTriangular(recolectarEstimaciones(scan, "Inf3")));
+        Inf.add(simularTriangular(recolectarEstimaciones(scan, "Inf4")));
+        Inf.add(simularTriangular(recolectarEstimaciones(scan, "Inf5")));
+        double TasImpuestos = 0.5;
+        double FDI [] = new double[5];
     }
     public static double simularTriangular(double[] estimaciones){
         double a = estimaciones[0];
@@ -33,8 +36,9 @@ public class Main {
         }
     }
 
-    public static double[] recolectarEstimaciones(Scanner scan){
+    public static double[] recolectarEstimaciones(Scanner scan, String variable){
         double [] datos = new double[3];
+        System.out.println("Ingrese estimaciones " + variable);
         System.out.println("Pesimista: ");
         datos[0] = scan.nextDouble();
         System.out.println("Probable: ");
@@ -42,6 +46,10 @@ public class Main {
         System.out.println("Optimista: ");
         datos[2]  = scan.nextDouble();
         return datos;
+    }
+
+    public static double calcularFDI(double FAI, ArrayList<Double>Inf, double T, double AF, double AC){
+        return 1;
     }
 
 }
